3aef6e6e680f2ad8a1f33032727a667f743953d0
unknown <mahimaagarwal285@gmail.com>
2025-03-12 17:20:07 +0545

    Make Camera Movement Smoother

--- mahima-godot/scripts/player.cs
+++ mahima-godot/scripts/player.cs
@@ -3,24 +3,25 @@ using System;
 
 public partial class player : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+	[Export] public float Speed = 5.0f;
+	[Export] public float JumpVelocity = 4.5f;
 
 	[Export] public float TiltLowerLimit = Mathf.DegToRad(-90.0f); //min for clamp
 	[Export] public float TiltUpperLimit = Mathf.DegToRad(90.0f); //max for clamp
 	[Export] public Camera3D Camera;
-	    [Export] public float MouseSensitivity = 0.1f; // Mouse sensitivity for camera control
+	[Export] public float MouseSensitivity = 0.05f; // Mouse sensitivity for camera control
 	
 	public Vector3 MouseRotation = new Vector3(0.0f,0.0f,0.0f);
-	public Vector3 CameraRotation= new Vector3(0.0f,0.0f,0.0f);
 	
 	public Boolean MouseInput = false;
 	public float AxisX;
 	public float AxisY;
 
+	public Vector3 PlayerRotation;
+	public Vector3 CameraRotation;
+
 	public override void _Ready()
 	{			
-		Input.MouseMode = Input.MouseModeEnum.Visible;
 		Input.MouseMode = Input.MouseModeEnum.Captured;
 	}
 
@@ -38,14 +39,14 @@ public partial class player : CharacterBody3D
 
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
 		{
 			velocity.Y = JumpVelocity;
 		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 inputDir = Input.GetVector("left", "right", "up", "down");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
 		{
@@ -69,8 +70,8 @@ public partial class player : CharacterBody3D
 			if(Input.MouseMode == Input.MouseModeEnum.Captured)
 			{
 				MouseInput = true;
-				AxisX = -mouseEvent.Relative.X * MouseSensitivity;
-				AxisY = -mouseEvent.Relative.Y * MouseSensitivity;
+				AxisX = -mouseEvent.Relative.Y * MouseSensitivity;
+				AxisY = -mouseEvent.Relative.X * MouseSensitivity;
 				GD.Print(AxisX," ",AxisY);
 			}
 			else
@@ -87,12 +88,17 @@ public partial class player : CharacterBody3D
 
 	public void UpdateCamera(double delta)
 	{
-		MouseRotation.X += (float)(AxisX * delta);
-		MouseRotation.X = Mathf.Clamp(MouseRotation.X,TiltLowerLimit, TiltUpperLimit);
 		MouseRotation.Y += (float)(AxisY * delta); 
+
+		MouseRotation.X += (float)(AxisX * delta);
+		MouseRotation.Y = Mathf.Clamp(MouseRotation.Y,TiltLowerLimit, TiltUpperLimit);
+
+        PlayerRotation = new Vector3(0.0f, MouseRotation.Y, 0.0f);
+		CameraRotation = new Vector3(MouseRotation.X, 0.0f, 0.0f);
 
 		Camera.Basis = Basis.FromEuler(MouseRotation);
 		//Camera.Rotation = new Vector3(MouseRotation.X, MouseRotation.Y, 0.0f);
+		Camera.Rotation = new Vector3(Mathf.LerpAngle(Camera.Rotation.X, MouseRotation.X, 0.1f), MouseRotation.Y, 0.0f);
 
 		AxisX= 0.0f;
 		AxisY= 0.0f;
