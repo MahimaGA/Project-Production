b329c175e212ee47f6c72f103258ca1b585cb57a
unknown <mahimaagarwal285@gmail.com>
2025-04-12 19:40:13 +0545

    Add Mediapipe to display landmarks

--- mahima-opencv/hand_detection.py
+++ mahima-opencv/hand_detection.py
@@ -1,8 +1,18 @@
 import cv2
 import mediapipe as mp
 
+mphands = mp.solutions.hands
+hands = mphands.Hands(
+    static_image_mode= False,
+    model_complexity= 1,
+    min_detection_confidence= 0.7, #70%
+    min_tracking_confidence= 0.7,
+    max_num_hands = 1
+)
+
 def main():
     capture = cv2.VideoCapture(0) #using primary camera=0
+    draw = mp.solutions.drawing_utils #draws landmarks
 
     try:
         while capture.isOpened():
@@ -11,6 +21,20 @@ def main():
             if not ret:
                 break
             frame=cv2.flip(frame, 1) #mirroring
+            frameRGB= cv2.cvtColor(frame,cv2.COLOR_BGR2RGB) #bluegreenred to RGB
+            
+            processed = hands.process(frameRGB)
+
+            landmarks_list = []
+
+            if processed.multi_hand_landmarks:
+                hand_landmarks = processed.multi_hand_landmarks[0]
+                draw.draw_landmarks(frame, hand_landmarks, mphands.HAND_CONNECTIONS) #draw handlandmarks, connections
+
+                for lm in hand_landmarks.landmark: #taking each and appending
+                    landmarks_list.append((lm.x, lm.y)) #add coordinates to list
+
+                print(landmarks_list) #to check
 
             cv2.imshow("Frame", frame) #display frame
             if cv2.waitKey(1) & 0xFF == ord('q'): #wait 1 ms and key Q 
