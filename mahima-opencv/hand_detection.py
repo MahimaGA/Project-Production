4b67de653db4911edb4ccf8a27a77779b34abdf1
unknown <mahimaagarwal285@gmail.com>
2025-04-13 22:25:39 +0545

    Add shoot gesture

--- mahima-opencv/hand_detection.py
+++ mahima-opencv/hand_detection.py
@@ -12,20 +12,39 @@ hands = mphands.Hands(
     max_num_hands = 1
 )
 
+def isshoot(landmarks_list, thumb_to_index):
+    return (util.getangle(landmarks_list[5], landmarks_list[6], landmarks_list[8]) > 90 and 
+            util.getangle(landmarks_list[9], landmarks_list[10], landmarks_list[12]) < 50 and
+            thumb_to_index > 75 )
+
 def move_mouse(index_finger_tip):
     if index_finger_tip is not None:
         x = int(index_finger_tip.x * pg.size().width) #x coordinate and screen width
         y= int(index_finger_tip.y * pg.size().height) #y coordinate and screen height
         pg.moveTo(x, y) #move to x,y on screen
 
-def detect_gestures(frame, landmarks_list, processed):
+def detect_gestures(frame, landmarks_list, processed, shooting_state):
     if len(landmarks_list) >= 21:
         
         index_finger_tip = find_tip(processed)
         thumb_to_index = util.getdistance([landmarks_list[4], landmarks_list[5]])
-
-        if thumb_to_index < 50 and util.getangle(landmarks_list[5], landmarks_list[6], landmarks_list[8]) > 90:
+        
+        #aim (curosr movement)
+        if thumb_to_index < 75 and util.getangle(landmarks_list[5], landmarks_list[6], landmarks_list[8]) > 90:
             move_mouse(index_finger_tip)
+            shooting_state[0] = False
+            cv2.putText(frame, "Aiming", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+        #shoot (enter key)
+        elif isshoot(landmarks_list, thumb_to_index):
+            if not shooting_state[0]:  # only shoot on first detection
+                pg.press('enter')
+                shooting_state[0] = True            
+                cv2.putText(frame, "Shooting", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        else:
+            shooting_state[0] = False
+            cv2.putText(frame, "Idle", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
 def find_tip(processed):
     if processed.multi_hand_landmarks:
@@ -38,9 +57,15 @@ def main():
     capture = cv2.VideoCapture(0) #using primary camera=0
     draw = mp.solutions.drawing_utils #draws landmarks
 
+    #resolution size
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+    shooting = [False] #list form so that we can change inside a function
+
     try:
         while capture.isOpened():
-            ret, frame = capture.read() #return(t/f), framedata
+            ret, frame = capture.read() #return(true/false), framedata
 
             if not ret:
                 break
@@ -58,7 +83,7 @@ def main():
                 for lm in hand_landmarks.landmark: #taking each and appending
                     landmarks_list.append((lm.x, lm.y)) #add coordinates to list
 
-            detect_gestures(frame, landmarks_list, processed)
+            detect_gestures(frame, landmarks_list, processed, shooting)
 
                 #print(landmarks_list) #to check
 
