cdb0e47ee7667e54a4a012fb07a54c599bf9cfa6
unknown <mahimaagarwal285@gmail.com>
2025-05-10 17:31:14 +0545

    Add proper UI to 2D elements

--- mahima-godot/scripts/gamecomplete.cs
+++ mahima-godot/scripts/gamecomplete.cs
@@ -2,28 +2,37 @@ using Godot;
 using System.Linq;
 using System.Collections.Generic;
 
-
 public partial class gamecomplete : Control
 {
-	[Export] public Label allScoresLabel;
+    [Export] public Label allScoresLabel;
     [Export] public Label allTargetsLabel;
+    [Export] public Label totalScoreLabel;
+    [Export] public Label totalHitsLabel;
 
     public override void _Ready()
-    {   
-        var results    = GameManager.Instance.LevelResults;
-        var levelKeys  = results.Keys.OrderBy(level => level);
+    {
+        var results = GameManager.Instance.LevelResults;
+        var levelKeys = results.Keys.OrderBy(level => level);
 
         var scores = new List<string>();
-        var hits   = new List<string>();
+        var hits = new List<string>();
+
+        int totalScore = 0;
+        int totalHits = 0;
 
         foreach (var lvl in levelKeys)
         {
             var r = results[lvl];
             scores.Add(r.Score.ToString());
             hits.Add(r.Hits.ToString());
+
+            totalScore += r.Score;
+            totalHits += r.Hits;
         }
 
-        allScoresLabel.Text  = string.Join("\n", scores);
+        allScoresLabel.Text = string.Join("\n", scores);
         allTargetsLabel.Text = string.Join("\n", hits);
+        totalScoreLabel.Text =totalScore.ToString();
+        totalHitsLabel.Text = totalHits.ToString();
     }
 }
